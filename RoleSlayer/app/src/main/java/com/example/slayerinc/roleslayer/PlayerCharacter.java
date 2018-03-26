397da30d297c4676dd16d360c6852b2aef9b69f7
vincentcote87 <vincentcote87@gmail.com>
2018-03-26 13:34:36 -0600

    Started a character class

---
+++ RoleSlayer/app/src/main/java/com/example/slayerinc/roleslayer/PlayerCharacter.java
@@ -0,0 +1,17 @@
+package com.example.slayerinc.roleslayer;
+
+/**
+ * Created by vincentcote on 2018-03-26.
+ */
+
+public class PlayerCharacter {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+}
