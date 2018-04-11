92fa0f07b6a2beee690e6e9890359ab40ff71960
Vincent <vincentcote87@gmail.com>
2018-04-11 03:25:03 -0600

    added character main page, basics btn goes to basics page

---
+++ RoleSlayer/app/src/main/java/com/roleslayin/roleslayer/CharacterBasics.java
@@ -0,0 +1,13 @@
+package com.roleslayin.roleslayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterBasics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_basics);
+    }
+}
