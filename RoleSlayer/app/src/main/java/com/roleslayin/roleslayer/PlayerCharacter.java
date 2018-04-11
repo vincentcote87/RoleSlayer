681ffe777881481a3bcdcef5bd6f4dc32a329a33
Vincent <vincentcote87@gmail.com>
2018-04-11 03:49:38 -0600

    need to pass object through intent in mainactivity

--- RoleSlayer/app/src/main/java/com/roleslayin/roleslayer/PlayerCharacter.java
+++ RoleSlayer/app/src/main/java/com/roleslayin/roleslayer/PlayerCharacter.java
@@ -2,12 +2,19 @@ package com.roleslayin.roleslayer;
 
 public class PlayerCharacter {
     String name;
+    String info;
 
     void setName(String name) {
         this.name = name;
     }
+    void setInfo(String info) {
+        this.info = info;
+    }
 
     String getName() {
         return name;
+    }
+    String getInfo() {
+        return info;
     }
 }
