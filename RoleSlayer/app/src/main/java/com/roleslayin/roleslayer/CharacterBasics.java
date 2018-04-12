c41c5c05891c62d4c9a67e689153f4f7c8e07f29
Vincent <vincentcote87@gmail.com>
2018-04-11 23:37:00 -0600

    chamged basicspage layout

--- RoleSlayer/app/src/main/java/com/roleslayin/roleslayer/CharacterBasics.java
+++ RoleSlayer/app/src/main/java/com/roleslayin/roleslayer/CharacterBasics.java
@@ -22,26 +22,19 @@ public class CharacterBasics extends AppCompatActivity implements EditField.Edit
     private String attribute;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_basics);
 
-//        savedExtra = getIntent();
         index = getIntent().getIntExtra("index", 0);
         setText();
-//        final PlayerCharacter playerChar = savedExtra.getParcelableExtra("myChar");
 
-
-        name = findViewById(R.id.nameID);
+        name = findViewById(R.id.nameLabelID);
         name.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditField nameField = new EditField();
-//                myArgs = new Bundle();
-//                myArgs.putString("header_id", "Edit Character Name");
-//                nameField.setArguments(myArgs);
                 nameField.show(getSupportFragmentManager(), "Edit name");
                 attribute = "name";
             }
@@ -113,55 +106,62 @@ public class CharacterBasics extends AppCompatActivity implements EditField.Edit
         tmpStr = field;
         Integer i;
         switch (attribute) {
-            case "name" :
-                ((MyApplication)getApplicationContext()).characterArray.get(index).setName(tmpStr); break;
-            case "race" :
-                ((MyApplication)getApplicationContext()).characterArray.get(index).setRace(tmpStr); break;
-            case "age" :
+            case "name":
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setName(tmpStr);
+                break;
+            case "race":
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setRace(tmpStr);
+                break;
+            case "age":
                 try {
                     i = Integer.parseInt(tmpStr);
                 } catch (NumberFormatException nfe) {
                     i = 0;
                 }
-                 ((MyApplication)getApplicationContext()).characterArray.get(index).setAge(i); break;
-            case "sex" :
-                ((MyApplication)getApplicationContext()).characterArray.get(index).setSex(tmpStr); break;
-            case "HT" :
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setAge(i);
+                break;
+            case "sex":
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setSex(tmpStr);
+                break;
+            case "HT":
                 try {
                     i = Integer.parseInt(tmpStr);
                 } catch (NumberFormatException nfe) {
                     i = 0;
                 }
-                ((MyApplication)getApplicationContext()).characterArray.get(index).setHT(i); break;
-            case "WT" :
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setHT(i);
+                break;
+            case "WT":
                 try {
                     i = Integer.parseInt(tmpStr);
                 } catch (NumberFormatException nfe) {
                     i = 0;
                 }
-                ((MyApplication)getApplicationContext()).characterArray.get(index).setWT(i); break;
-            case "description" :
-                ((MyApplication)getApplicationContext()).characterArray.get(index).setDescription(tmpStr); break;
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setWT(i);
+                break;
+            case "description":
+                ((MyApplication) getApplicationContext()).characterArray.get(index).setDescription(tmpStr);
+                break;
         }
         setText();
 
     }
 
     private void setText() {
-        TextView nameText = (TextView) findViewById(R.id.nameID);
+        TextView nameText = (TextView) findViewById(R.id.nameInputID);
         TextView raceText = (TextView) findViewById(R.id.raceInputID);
         TextView ageText = (TextView) findViewById(R.id.ageInputID);
         TextView sexText = (TextView) findViewById(R.id.sexInputID);
         TextView htText = (TextView) findViewById(R.id.htInputID);
         TextView wtText = (TextView) findViewById(R.id.wtInputID);
         TextView descriptionText = (TextView) findViewById(R.id.descriptionInputID);
-        nameText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getName());
-        raceText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getRace());
-        ageText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getAge().toString());
-        sexText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getSex());
-        htText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getHT().toString());
-        wtText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getWT().toString());
-        descriptionText.setText(((MyApplication)this.getApplicationContext()).characterArray.get(index).getDescription());
+        nameText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getName());
+        raceText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getRace());
+        ageText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getAge().toString());
+        sexText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getSex());
+        htText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getHT().toString());
+        wtText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getWT().toString());
+        descriptionText.setText(((MyApplication) this.getApplicationContext()).characterArray.get(index).getDescription());
     }
 
     @Override
