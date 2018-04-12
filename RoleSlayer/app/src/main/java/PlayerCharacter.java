15fbc10e3aefa3f6f4fa91ec75a33bc37c644369
Winterstone <graham.panchuk@uleth.ca>
2018-04-11 21:03:05 -0600

    Added PlayerCharacter stat setters and made it Parcelable, began Skill methods

--- RoleSlayer/app/src/main/java/PlayerCharacter.java
+++ RoleSlayer/app/src/main/java/PlayerCharacter.java
@@ -1,10 +1,14 @@
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * Created by vincentcote on 2018-03-26.
  */
 
-public class PlayerCharacter {
+public class PlayerCharacter implements Parcelable
+{
     // Data Members
 
     // Constants
@@ -43,6 +47,11 @@ public class PlayerCharacter {
 
     }
 
+    private PlayerCharacter(Parcel in) {
+        name = in.readString();
+        // ETC
+    }
+
     PlayerCharacter(String n)
     {
         name = n;
@@ -62,8 +71,105 @@ public class PlayerCharacter {
         basic_lift = (st * st) / 5;
     }
 
+    void calcSkills(String stat, int score)
+    {
+        // Loop through skills looking for stat == attribute,
+        // And call skill.calculate(score)
+    }
+
+    void calcDmg()
+    {
+        // Do I just use a table/big long switch I guess?
+    }
+
     void setSt(int score)
     {
+        int diff = score - st;
+        build_points += diff * ST_PTS;
+        st = score;
+        calcLift();
+        calcDmg();
+        setHp(hp + diff);
+    }
+
+    void setDx(int score)
+    {
+        int diff = score - dx;
+        build_points += diff * DX_PTS;
+        dx = score;
+        calcSpeed();
+        calcSkills("dx", dx);
+    }
+
+    void setIq(int score)
+    {
+        int diff = score - iq;
+        build_points += diff * IQ_PTS;
+        iq = score;
+        calcSkills("iq", iq);
+        setWil(wil + diff);
+        setPer(per + diff);
+    }
+
+    void setHt(int score)
+    {
+        int diff = score - ht;
+        build_points += diff * HT_PTS;
+        ht = score;
+        calcSpeed();
+        calcSkills("ht", ht);
+        setFp(fp + diff);
+    }
+
+    void setHp(int score)
+    {
+        int diff = score - hp;
+        build_points += diff * HP_PTS;
+        hp = score;
+    }
+
+    void setWil(int score)
+    {
+        int diff = score - wil;
+        build_points += diff * WIL_PTS;
+        wil = score;
+        calcSkills("wil", wil);
+    }
+
+    void setPer(int score)
+    {
+        int diff = score - per;
+        build_points += diff * PER_PTS;
+        per = score;
+        calcSkills("per", per);
+    }
+
+    void setFp(int score)
+    {
+        int diff = score - fp;
+        build_points += diff * FP_PTS;
+        fp = score;
+    }
+
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
 
     }
+
+    public static final Parcelable.Creator<PlayerCharacter> CREATOR = new Parcelable.Creator<PlayerCharacter>() {
+        public PlayerCharacter createFromParcel(Parcel in) {
+            return new PlayerCharacter(in);
+        }
+        public PlayerCharacter[] newArray(int size) {
+            return new PlayerCharacter[size];
+        }
+    };
+
 }
--- RoleSlayer/app/src/main/java/Skill.java
+++ RoleSlayer/app/src/main/java/Skill.java
@@ -2,9 +2,29 @@ public class Skill {
     // Data members
 
     String name, attribute;
-    int difficulty, level;
+    int difficulty, relative_level, level;
 
     // Methods
+
+    Skill(String nm, String stat, int diff, int base)
+    {
+        name = nm;
+        attribute = stat;
+        difficulty = diff;
+        relative_level = rlInit(difficulty);
+        // 1 build point -- how to pass back?
+        calculate(base);
+    }
+
+    void calculate(int base)
+    {
+        level = relative_level + base;
+    }
+
+    int rlInit(int diff)
+    {
+        return 1 - diff;
+    }
 
 
 }
