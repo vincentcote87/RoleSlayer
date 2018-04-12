25610d2e8371c0b5e986ac9b53e463877af5d15a
Winterstone <graham.panchuk@uleth.ca>
2018-04-12 15:22:12 -0600

    Stats page layout

---
+++ RoleSlayer/app/src/main/java/com/roleslayin/roleslayer/CharacterStats.java
@@ -0,0 +1,6 @@
+package com.roleslayin.roleslayer;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CharacterStats extends AppCompatActivity implements EditField.EditFieldListener {
+}
